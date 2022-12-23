6d485b582701fe50026432b45cdf5c6c40c8a5de
rubbymf <114345875+rubbymf@users.noreply.github.com>
2022-12-23 19:18:12 +0700

    Add files via upload

---
+++ Mahasiswaku.java
@@ -0,0 +1,15 @@
+package sesi9;
+public class Mahasiswaku {
+    
+    public static void main(String[] args) {
+        Mahasiswa <String,String, Integer> m = new Mahasiswa<>();
+        m. setNim("1102020");
+        m. setName ("Ferdi");
+        m. setClas(21);
+        System.out.println(m.getNim());
+        System.out.println(m.getName ());
+        System.out.println(m.getClas());
+    }
+}
+
+//Maka buatlah contoh progam genericnya agar kode progam diatas dapat berjalan?
